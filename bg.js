56b870974d4fdf47dbd5fb11fa9eae717a16348f
zmni <zmni@users.noreply.github.com>
2022-09-17 14:02:48 +0700

    update to mv3

--- bg.js
+++ bg.js
@@ -1,10 +1,13 @@
+// Context menu create
 chrome.contextMenus.create({
+    id: "searchOnYouTube",
     title: chrome.i18n.getMessage("contextMenuTitle") + ' "%s"',
     contexts: ["selection"],
-    onclick: (e) => {
-        const term = decodeURIComponent(e.selectionText).replace(/\s+/g, "+");
-        chrome.tabs.create({
-            url: "https://www.youtube.com/results?search_query=" + term,
-        });
-    },
+});
+
+// Context menu click handler
+chrome.contextMenus.onClicked.addListener((e) => {
+    chrome.tabs.create({
+        url: "https://www.youtube.com/results?search_query=" + decodeURIComponent(e.selectionText).replace(/\s+/g, "+"),
+    });
 });
